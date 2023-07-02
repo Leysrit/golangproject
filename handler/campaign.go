2a4b7e27e82670059b91fb0f9f5e2939d3ea128d
Hatta56 <hattasugiarto443@gmail.com>
2023-07-02 16:19:49 +0700

    get campaign endpoint formatter

--- handler/campaign.go
+++ handler/campaign.go
@@ -19,14 +19,14 @@ func NewCampaignHandler(service campaign.Service) *campaignHandler {
 func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	userID, _ := strconv.Atoi(c.Query("user_id"))
 
-	campaign, err := h.service.GetCampaigns(userID)
+	campaigns, err := h.service.GetCampaigns(userID)
 	if err != nil {
-		response := helper.APIResponse("error to get campaigns", http.StatusBadRequest, "error", nil)
+		response := helper.APIResponse("error to get campaigns", http.StatusBadRequest, "error", campaign.FormatCampaigns(campaigns))
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("success to get campaigns", http.StatusOK, "success", campaign)
+	response := helper.APIResponse("success to get campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 
 }
